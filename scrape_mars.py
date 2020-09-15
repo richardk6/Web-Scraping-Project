6392c4963665390b9e4fa82cd92bde870efaa79e
Kelsey Richardson <kelseyrichardson6@gmail.com>
2020-09-15 17:44:46 -0500

    final push

--- scrape_mars.py
+++ scrape_mars.py
@@ -12,18 +12,10 @@ def init_browser():
 def scrape_all():
     browser = init_browser()
     mars_dict = {}
-    news_info = mars_news()
-    mars_dict["mars_title"] = news_info[0]
-    mars_dict["mars_p"] = news_info[1]
-    mars_dict["mars_image"] = mars_image()
-    mars_dict["mars_facts"] = mars_facts()
-    mars_dict["hemisphere_image_urls"] = hemispheres()
-
-    return mars_dict
 
 # Mars News
 
-def mars_news():
+# def mars_news():
     browser = init_browser()
     url = "https://mars.nasa.gov/news/"
     browser.visit(url)
@@ -31,18 +23,22 @@ def mars_news():
     time.sleep(10)
 
     html = browser.html
-    soup = bs(html, "html.parser")
+    title_news_soup = bs(html, "html.parser")
 
-    title = soup.find("ul", class_="item_list")
-    news_title = title.find("div", class_="content_title")
+    try:
+        slide_elem = title_news_soup.select_one("ul.item_list li.slide")
 
-    news_p = soup.find("div", class_="article_teaser_body")
+        news_title = slide_elem.find("div", class_="content_title")
+        news_p = slide_elem.find("div", class_="article_teaser_body")
+    
+    except AttributeError:
+        return None, None
 
-    news_info = [news_title, news_p]
-    return news_info
+    mars_dict["mars_title"] = news_title
+    mars_dict["mars_p"] = news_p
 
 # Mars Featured Image
-def mars_image():
+# def mars_image():
     browser = init_browser()
     url = "https://www.jpl.nasa.gov/spaceimages/?search=&category=Mars"
     browser.visit(url)
@@ -62,11 +58,11 @@ def mars_image():
 
     featured_url = "https://www.jpl.nasa.gov" + featured_image_url
 
-    return featured_url
+    mars_dict["mars_image"] = featured_url
 
 
 # Mars Facts Table
-def mars_facts():
+# def mars_facts():
     # Add try/except for error handling
     try:
         # use 'read_html' to scrape the facts table into a dataframe
@@ -77,50 +73,25 @@ def mars_facts():
     df.columns = ['Description', 'Mars']
     df.set_index('Description', inplace=True)
     # Convert dataframe into HTML format, add bootstrap
-    return df.to_html(classes="table table-striped")
+    mars_table = df.to_html(classes="table table-striped")
+
+    mars_dict["mars_facts"] = mars_table
 
 # Mars Hemispheres 
-def hemispheres():
+# def hemispheres():
     browser = init_browser()
 
     # Visit website
     url = 'https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars'
     browser.visit(url)
 
-    response = requests.get(url)
-    soup = bs(response.text, 'html.parser').find_all("a",class_ = "itemLink product-item")
-    hemi_titles = []
-    for i in soup:
-        title = i.find("h3").text
-        hemi_titles.append(title)
-        print(title)
-
-    hemi_imglist = []
-
-    for x in range(len(hemi_titles)):
-        try:
-            browser.click_link_by_partial_text(hemi_titles[x])
-        except:
-            browser.find_link_by_text('2').first.click()
-            browser.click_link_by_partial_text(hemi_titles[x])
-            html = browser.html
-            soup2 = bs(html, 'html.parser')
-            hemi_soup = soup2.find('div', 'downloads')
-            hemi_url = hemi_soup.a['href']
-            #urls.append(hemi_url)
-            hemi_dict={"title": hemi_titles[x], 'img_url': hemi_url}
-            hemi_imglist.append(hemi_dict)
-            print(hemi_imglist)
-
-            # Find the src for the image
-            hemisphere_image_urls = [
-            {'title': 'Cerberus Hemisphere Enhanced', 'img_url': 'https://astropedia.astrogeology.usgs.gov/download/Mars/Viking/cerberus_enhanced.tif/full.jpg'}, 
-            {'title': 'Schiaparelli Hemisphere Enhanced', 'img_url': 'https://astropedia.astrogeology.usgs.gov/download/Mars/Viking/schiaparelli_enhanced.tif/full.jpg'}, 
-            {'title': 'Syrtis Major Hemisphere Enhanced', 'img_url': 'https://astropedia.astrogeology.usgs.gov/download/Mars/Viking/syrtis_major_enhanced.tif/full.jpg'}, 
-            {'title': 'Valles Marineris Hemisphere Enhanced', 'img_url': 'https://astropedia.astrogeology.usgs.gov/download/Mars/Viking/valles_marineris_enhanced.tif/full.jpg'}
-            ]
+    hemisphere_image_urls = []
+    hemi_data = browser.find_by_css("a.product-item h3")[1].click()
+    hemisphere_image_urls.append(hemi_data)
     
-    return hemisphere_image_urls
+    mars_dict["hemisphere_image_urls"] = hemisphere_image_urls
+
+    return mars_dict
 
     # Close the browser after scraping
     browser.quit()
